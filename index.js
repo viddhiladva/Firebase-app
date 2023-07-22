a394b123b383d3219da2533affcff24fd31e030a
Viddhi Ladva <vidhiladva@gmail.com>
2023-07-22 15:56:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-\\https://console.firebase.google.com/u/0/project/realtime-database-c7ff6/database/realtime-database-c7ff6-default-rtdb/data
+// https://console.firebase.google.com/u/0/project/realtime-database-c7ff6/database/realtime-database-c7ff6-default-rtdb/data
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
